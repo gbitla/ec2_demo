b50a9d1bfa8780769b47f5285301a90df224f535
Girish Bitla <gbitla@salesforce.com>
2018-07-03 10:49:26 -0700

    Intial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  count         = 3
+  ami           = "ami-2d39803a"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "terraform_example_node"
+  }
+}
