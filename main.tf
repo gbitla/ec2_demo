3afb3260f41076828fee4050d2e016a7166a2599
Girish Bitla <gbitla@salesforce.com>
2018-07-03 12:28:05 -0700

    added security group and user_data to run httpd in the background

--- main.tf
+++ main.tf
@@ -4,10 +4,28 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   #count         = 3
-  ami           = "ami-2d39803a"
-  instance_type = "t2.micro"
+  ami                    = "ami-2d39803a"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance-sg.id}"]
+
+  user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
 
   tags {
     Name = "terraform_example_node"
+  }
+}
+
+resource "aws_security_group" "instance-sg" {
+  name = "terraform-example-instance-sg"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
